1adb0060870626ff65259c5fe23c3545a01f5c48
ayatk <git@ayatk.com>
2017-12-25 11:30:33 +0900

    Delete Java servlet files

--- src/main/java/com/ayatk/webcom/db/Config.java
+++
@@ -1,8 +0,0 @@
-package com.ayatk.webcom.db;
-
-public class Config {
-    public static String user = "default";
-    public static String password = "secret";
-    public static String driver = "com.mysql.cj.jdbc.Driver";
-    public static String url = "jdbc:mysql://localhost:3306/default?useSSL=false";
-}
