373e630e7b1adeca9a9dc5fd803ba376fa42d247
ayatk <git@ayatk.com>
2017-12-29 17:13:18 +0900

    Apply milligram.css

--- index.php
+++ index.php
@@ -4,14 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
+    <link rel="stylesheet" href="static/css/normalize.css">
+    <link rel="stylesheet" href="static/css/milligram.min.css">
+
     <title>Document</title>
 </head>
 <body>
-<p id="date">
-    <?php
-    date_default_timezone_set('Asia/Tokyo');
-    echo date('l jS \of F Y h:i:s A');
-    ?>
-</p>
+<div class="container">
+    <p id="date">
+        <?php
+        date_default_timezone_set('Asia/Tokyo');
+        echo date('l jS \of F Y h:i:s A');
+        ?>
+    </p>
+</div>
 </body>
 </html>
